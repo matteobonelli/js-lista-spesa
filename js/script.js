7ef2cb9b3f7710041dbb91e146e159ba02ed441a
matteobonelli <matteobonelli08@gmail.com>
2023-10-16 16:55:14 +0200

    now a message is visualized when you can't insert you shopping list

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const btn = document.querySelector('button');
 const userInput = document.getElementById('data');
-const alertOutput = document.querySelector('.alert');
-
+const alertOutput = document.querySelector('.messaggio');
+const alertSpesa = document.querySelector('.alert-danger');
 let container = [];
-
+console.log(alertSpesa);
 btn.addEventListener('click' , function(){
     spesaEl = userInput.value;
     if(spesaEl.toLowerCase() === 'stop'){
         btn.classList.add('d-none');
+        alertSpesa.innerText = 'Non puoi più inserire la tua spesa';
+        alertSpesa.classList.remove('d-none');
         alertOutput.innerText = container;
     } else{
         container.push(spesaEl + ' ');
@@ -18,5 +20,6 @@ btn.addEventListener('click' , function(){
     console.log(container);
     alertOutput.classList.add('alert-success');
     alertOutput.classList.remove('d-none');
+    spesaEl = userInput.value = '';
 })
 
